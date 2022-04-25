78758ceb647784677ee7a7aad7d8ddeea80d0325
mazzabodwin-dit <mary.bowden@digital.trade.gov.uk>
2022-04-25 17:46:05 +0100

    drag and drop, upoads and more

--- app/assets/javascripts/drag_and_drop.js
+++ app/assets/javascripts/drag_and_drop.js
@@ -1,5 +1,40 @@
 const pause = _ => new Promise(resolve => setTimeout(resolve, _))
 
+
+function file_upload(upload_container, file_name){
+  upload_container.find('.file_name').html(file_name)
+  upload_container.find('.waiting_for_upload').hide()
+  upload_container.find('.scanning_file_for_virus').show()
+  setTimeout(function () {
+    upload_container.find('.scanning_file_for_virus').hide()
+    upload_container.find('.uploading_file').show()
+
+    setTimeout(function () {
+      upload_container.find('progress').hide()
+      upload_container.find('.progress-40').show()
+      upload_container.find('.upload_percentage').html('Uploading 40%:')
+      setTimeout(function () {
+        upload_container.find('progress').hide()
+        upload_container.find('.progress-70').show()
+        upload_container.find('.upload_percentage').html('Uploading 70%:')
+        setTimeout(function () {
+          upload_container.find('progress').hide()
+          upload_container.find('.progress-100').show()
+          upload_container.find('.upload_percentage').html('Uploading 100%:')
+          upload_container.find('.uploading_file').hide()
+          upload_container.find('.upload_file_complete').show()
+        }, 1000)
+      }, 1000)
+    }, 1000)
+  }, 1000)
+}
+
+$('input[type="file"]').change(function(){
+  let file_name = $(this).val().split(/(\\|\/)/g).pop()
+  let upload_container = $(this).closest('.upload-container')
+  file_upload(upload_container, file_name)
+})
+
 $('.upload-container').on('drop dragdrop', function (ev) {
   $(this).closest('.upload-card').removeClass('upload-card-hover')
   let original_event = ev.originalEvent
@@ -8,36 +43,7 @@ $('.upload-container').on('drop dragdrop', function (ev) {
   console.log('dropped')
 
   let file_name = original_event.dataTransfer.files[0].name
-  $(this).find('.file_name').html(file_name)
-
-  $(this).find('.waiting_for_upload').hide()
-  $(this).find('.scanning_file_for_virus').show()
-  let that = $(this)
-  setTimeout(function () {
-    that.find('.scanning_file_for_virus').hide()
-    that.find('.uploading_file').show()
-
-    setTimeout(function () {
-      that.find('progress').hide()
-      that.find('.progress-40').show()
-      that.find('.upload_percentage').html('Uploading 40%:')
-      setTimeout(function () {
-        that.find('progress').hide()
-        that.find('.progress-70').show()
-        that.find('.upload_percentage').html('Uploading 70%:')
-        setTimeout(function () {
-          that.find('progress').hide()
-          that.find('.progress-100').show()
-          that.find('.upload_percentage').html('Uploading 100%:')
-          that.find('.uploading_file').hide()
-          that.find('.upload_file_complete').show()
-        }, 500)
-      }, 500)
-    }, 500)
-
-
-  }, 1000)
-
+  file_upload($(this), file_name)
 })
 
 $('.upload-container').on('dragover', function (e) {
