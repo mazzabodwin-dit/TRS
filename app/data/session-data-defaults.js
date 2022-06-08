cee9ae8a733682706868ae962d63d3f3574f3a4e
mazzabodwin-dit <mary.bowden@digital.trade.gov.uk>
2022-06-08 16:55:49 +0100

    Caseworker - data and orgs

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -21,23 +21,46 @@ module.exports = {
 
   // Insert values here
 
+
+   //Key users 
   "publicname": "Bob Hoskins",
   "caseworkername": "Jonanna Barton",
   
+   //CASE 1
   "case-ref1": "AD2224",
   "case-name1": "Steel plate from Russia",
   "case-typeabr1": "AD",
   "case-type1": "Anti-dumping",
   "commodity1": "Steel plate",
-  "applicant1": "Steelman Plc",
+  "country1": "Russia",
   "created1": "12 May 2022",
   "initiated1": "13 May 2022",
   "register-date1": "01 June 2022",
   "updated1": "14 May 2022",
+  "status1": "Questionnaires issued",
+  "next-task1": "Publish questionnaires",
+  "next-notice1": "26 July 2022",
+
+  "applicant1": "Steelman Plc",
+  "app-user1": "Vladimir Boffoff",
+  "app-email1": "v.boffoff@Steelman.org",
+  "app-submission1": "Questionnaire",
+  "app-role1": "Producer",
+
 
   "party1": "Steel UK",
   "party-user1": "Simon Bellweather",
   "party-email1": "s.bellweather@steeluk.org",
+  "party-submission1": "Registration of Interest",
+  "party-country1": "United Kingdom",
+  "party-role1": "(Application sent)",
+
+  "party11": "StrongSteel",
+  "party-user11": "Martine Hulk",
+  "party-email11": "m.hulk@StrongSteel.org",
+  "party-submission11": "Representative invite",
+  "party-country11": "Russia",
+  "party-role11": "(Application sent)",
 
   "representative1": "BlueSteel Partners",
   "rep-user1": "Bob Hoskins",
@@ -45,17 +68,10 @@ module.exports = {
   "rep-role1": "Primary Admin",
   "rep-mobile1": "+44 7854329445",
   "rep-mobile-national1": "07854329445",
+  "rep-submission1": "Registration of Interest",
 
-  "country1": "Russia",
-  "submission1": "Registration of Interest",
-  "status1": "Questionnaires issued",
-  "next-task1": "Publish questionnaires",
-  "next-notice1": "26 July 2022",
-  "case-role1": "Domestic Producer",
-  "contact1": "Demi Morre",  
-  "contact-email1": "demi.morre@LawyersRUs.com",
-  "contact-role1": "(Invite sent)",
- 
+
+ //CASE 2 - case closed
   "case-ref2": "TS0002",
   "case-name2": "Rainbow Trout from Turkey",
   "case-typeabr2": "TAS",
@@ -66,43 +82,45 @@ module.exports = {
   "initiated2": "04 Mar 2019",
   "register-date2": "01 June 2019",
   "updated2": "30 Nov 2021",
-  "party2": "Trouty Turks",
-  "representative2": "??",
   "country2": "Republic of Turkey",
   "status2": "	Final determination",
   "next-task2": "n/a",
   "next-notice2": "n/a",
-  "case-role2": "Exporter",
-  "user2": "Clay Akin",
-  "contact2": "??",
+  
+  "party2": "Trouty Turks",
+  "party-role2": "Exporter",
+  "party-user2": "Clay Akin",
 
-
+//CASE 3
   "case-ref3": "AG0404",
   "case-name3": "Farnets from Fabon",
   "case-typeabr3": "AS",
   "case-type3": "Anti-subsidy investigation",
   "commodity3": "Farnets",
   "applicant3": "Farnets First UK",
+  "country3": "Federation of Fabon",
   "created3": "13 April 2022",
   "initiated3": "16 April 2022",
   "register-date3": "01 May 2022",
   "updated3": "16 April 2022",
-  "party3": "Fabulous Farnets",
-  "representative3": "BlueSteel Partners",
-  "country3": "Federation of Fabon",
   "status3": "Case initiated",
   "next-task3": "Issue questionnaires",
   "next-notice3": "01 June 2022",
-  "case-role3": "Importer",
-  "user3": "Bob Hoskins",
-  "user-email3": "bobhoskins@BlueSteelPartners.com",
-  "user-role3": "Primary Admin",
-  "user-mobile3": "+44 7854329445",
-  "contact3": "Simone Ward",
-  "contact-email3": "simone.ward@LawyersRUs.com",
-  "contact-role3": "Unassigned",
 
+  "party3": "Fabulous Farnets",
+  "party-user3": "Odessa Seas",
+  "party-email3": "o.seas@fabulous-farnets.fn",
+  "party-role3": "Importer",
 
+  "representative3": "Lawyers RUs",
+  "rep-user3": "Demi Morre",  
+  "rep-email3": "demi.morre@LawyersRUs.com",
+  "rep-role3": "(Invite sent)",
+  "rep-user31": "Simone Ward",
+  "rep-email31": "simone.ward@LawyersRUs.com",
+  "rep-role31": "Unassigned",
+
+//CASE 4
   "case-ref4": "BA0005",
   "case-name4": "Barnets from Tabon",
   "case-typeabr4": "SI",
@@ -113,19 +131,22 @@ module.exports = {
   "initiated4": "20 Nov 2020",
   "register-date4": "05 Dec 2020",
   "updated4": "23 Dec 2021",
-  "party4": "Best Barnets Corp",
-  "representative4": "BlueSteel Partners",
-  "country4": "United Tabon",
+  "country4": "United Kingdom",
   "status4": "Questionnaires issued",
   "next-task4": "Process questionnaires",
   "next-notice4": "02 Feb 2021",
-  "case-role4": "Importer",
-  "user4": "Jon Bodwina",
-  "user-email4": "jonbobina@BlueSteelPartners.com",
-  "user-role4": "De-activated",
-  "user-mobile4": "+44 73255436354",
-  "contact4": "Wally Were",
 
+  "party4": "Best Barnets Corp",
+  "party-user4": "Mohawks Fringe",
+  "party-email4": "m.fringe@best-barnets.uk",
+  "party-role4": "Importer",
+
+  "representative3": "Lawyers RUs",
+  "rep-user3": "Demi Morre",  
+  "rep-email3": "demi.morre@LawyersRUs.com",
+  "rep-role3": "Representative",
+
+  //CASE 5
   "case-ref5": "AS3464",
   "case-name5": "Garnets from Gabon",
   "case-typeabr5": "AS",
@@ -136,20 +157,23 @@ module.exports = {
   "initiated5": "01 Jan 2021",
   "register-date5": "15 Jan 2021",
   "updated5": "08 Jan 2021",
-  "party5": "Glorious Gabon Plc",
-  "representative5": "BlueSteel Partners",
   "country5": "Republic of Gabon",
   "status5": "Application received",
   "next-task5": "Initiation",
   "next-notice5": "01 Feb 2021",
   "case-role5": "Declined",
-  "user5": "Arnie Swarts",
-  "user-email5": "arnieswarts@BlueSteelPartners.com",
-  "user-role5": "Admin",
-  "user-mobile5": "+44 76345796334",
-  "contact5": "George Spinter",
 
-  "case-ref6": "SB9405",
+  "party5": "Glorious Gabon Plc",
+  "party-user5": "George Spinter",
+  "party-user5": "Spinter@glorgabon.gb",
+
+  //"representative1": "BlueSteel Partners",
+  //"rep-user1": "Bob Hoskins",
+  //"rep-email1": "bobhoskins@BlueSteelPartners.com",
+  
+
+  //CASE 6
+  "case-ref6": "SI9405",
   "case-name6": "Electric Bikes",
   "case-typeabr6": "SI",
   "case-type6": "Safeguarding Invesitgation",
@@ -159,18 +183,20 @@ module.exports = {
   "initiated6": "24 July 2021",
   "register-date6": "07 Aug 2021",
   "updated6": "24 July 2021",
-  "party6": "Power Bikes Ltd",
-  "representative6": "BlueSteel Partners",
   "country6": "Republic of China",
   "status6": "Initiation prepared",
   "next-task6": "Finalise case information",
   "next-notice6": "24 Sept 2021",
-  "case-role6": "Exporter",
-  "user6": "Bob Hoskins",
-  "user-email6": "bobhoskins@BlueSteelPartners.com",
-  "user-role6": "Primary Admin",
-  "user-mobile6": "+44 7854329445",
 
+  // This is their own case. Not representing another party
+  "party6": "BlueSteel Partners",
+  "party-user6": "Arnie Swarts",
+  "party-email6": "arnieswarts@BlueSteelPartners.com",
+  "submission6": "Registration of Interest",
+  "case-role6": "Exporter",
+
+
+//CASE 7 - DRAFT yet to be made public
   "case-ref7": "AD12234",
   "case-name7": "Corn-on-cob from Alaska",
   "case-typeabr7": "AD",
@@ -181,12 +207,9 @@ module.exports = {
   "initiated7": "n/a",
   "register-date7": "n/a",
   "updated7": "24 July 2021",
-  "party7": "Cob Farms",
-  "representative7": "?",
   "country7": "United States",
   "status7": "DRAFT",
   "next-task7": "n/a",
   "next-notice7": "n/a/",
-  "case-role7": "Importer",
-  "user7": "?",
+
 }
