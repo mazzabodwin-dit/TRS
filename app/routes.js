425758fe2ce7593b8078603c8f8ec9f3e64921f4
mazzabodwin-dit <100353757+mazzabodwin-dit@users.noreply.github.com>
2022-08-15 16:00:13 +0100

    Moved Caseworker invite / submissions to own folder and other invite tweaks

--- app/routes.js
+++ app/routes.js
@@ -300,25 +300,4 @@ router.post('/reginterest-loa-existing-route', function (req, res) {
   } 
 })
 
-
-// Run this code on invite-party-contacts-choice - which option is chosen:
-router.post('/invite-party-contacts-choice-route', function (req, res) {
-
-  // Make a variable and give it the value from 'reginterest-valid-party'
-  var invitePartyContactsChoice = req.session.data['invite-party-contacts-choice']
-
-  // Check whether the variable matches a condition
-  if (invitePartyContactsChoice == "new-contact"){
-    // Send user to next page
-    res.redirect('/caseworker/cases/invite-party-contacts')
-  } else {
-    // Send user to no page
-    res.redirect('/caseworker/cases/invite-party-check')
-  }
-})
-
-
-
-
-
 module.exports = router
