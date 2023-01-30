beafc3c8b2c7cd37a6ad999698b2c0d22a05910c
mazzabodwin-dit <100353757+mazzabodwin-dit@users.noreply.github.com>
2023-01-30 10:21:32 +0000

    Revert "multiple autocomplete now allowed on one page"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,43 +8,29 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 
-  let selectElements = $('#search-reg-org, .autocomplete-select')
+  let selectElement = $('#search-reg-org')
   let placeholder = 'Enter a registered company name or number'
-
-  selectElements.each(function (index) {
-    let selectElement = $(this)
-    if (selectElement.length) {
-      // AccessibleAutocomplete orignal
-      if (selectElement.attr('autocomplete-placeholder')) {
-        placeholder = selectElement.attr('autocomplete-placeholder')
-      }
-      accessibleAutocomplete.enhanceSelectElement({
-        defaultValue: '',
-        placeholder: placeholder,
-        selectElement: selectElement[0],
-        onConfirm: function (data) {
-          // First we check if the selectElement has a particular company-wrapper linked to it
-          let company_wrapper_id = 'selected_company_wrapper'
-          if (this.selectElement.hasAttribute('data-selected-company-wrapper-id')) {
-            company_wrapper_id = this.selectElement.getAttribute('data-selected-company-wrapper-id')
-          }
-          let company_wrapper_element = $('#' + company_wrapper_id)
-          let selected_company_element = company_wrapper_element.find("div.govuk-inset-text")
-
-          if (data !== undefined) {
-            // We only want to hide if the variable is defined
-            if (data) {
-
-              selected_company_element.html($('option:contains(' + data + ')').val())
-              company_wrapper_element.show()
-            } else {
-              company_wrapper_element.hide()
-            }
+  if (selectElement.length) {
+    // AccessibleAutocomplete orignal
+    if (selectElement.attr('autocomplete-placeholder')) {
+      placeholder = selectElement.attr('autocomplete-placeholder')
+    }
+    accessibleAutocomplete.enhanceSelectElement({
+      defaultValue: '',
+      placeholder: placeholder,
+      selectElement: document.querySelector('#search-reg-org'),
+      onConfirm: function (data) {
+        if (data !== undefined) {
+          // We only want to hide if the variable is defined
+          if (data) {
+            $('#selected_company').html($('option:contains(' + data + ')').val())
+            $('#selected_company_wrapper').show()
+          } else {
+            $('#selected_company_wrapper').hide()
           }
         }
-      })
-    }
-
-  })
+      }
+    })
+  }
 
 })
