91a2a1e1552982e34ae1e9d10556836a0dded5ce
mazzabodwin-dit <100353757+mazzabodwin-dit@users.noreply.github.com>
2023-03-30 10:39:09 +0100

    Autocomplete searches for caseworker users / orgs

--- app/assets/javascripts/application copy.js
+++ app/assets/javascripts/application copy.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
       if (selectElement.attr('autocomplete-placeholder')) {
         placeholder = selectElement.attr('autocomplete-placeholder')
       }
-        accessibleAutocomplete.enhanceSelectElement({
+      accessibleAutocomplete.enhanceSelectElement({
         defaultValue: '',
         placeholder: placeholder,
         selectElement: selectElement[0],
@@ -26,7 +26,7 @@ $(document).ready(function () {
 
           // First we check if the selectElement has a particular wrapper linked to it
 
-          // Search for company in Companies House search - setting the div for showing the option value
+          // Search for company in Companies House search
           let company_wrapper_id = 'selected_company_wrapper'
           if (this.selectElement.hasAttribute('data-selected-company-wrapper-id')) {
             company_wrapper_id = this.selectElement.getAttribute('data-selected-company-wrapper-id')
@@ -34,17 +34,10 @@ $(document).ready(function () {
           let company_wrapper_element = $('#' + company_wrapper_id)
           let selected_company_element = company_wrapper_element.find("div.govuk-inset-text")
 
-          // Search for case/ commodity in Public files search - setting the div for showing the option value
+          // Search for case/ commodity in Public files search
           let commodity_wrapper_id = 'selected_commodity_wrapper'
           let commodity_wrapper_element = $('#' + commodity_wrapper_id)
           let selected_commodity_element = commodity_wrapper_element.find("a#selected_commodity")
-
-          // Search for users in caseworker - setting the div for showing the option value
-          let user_wrapper_id = 'selected_user_wrapper'
-          let user_wrapper_element = $('#' + user_wrapper_id)
-          let selected_user_element = user_wrapper_element.find("a#selected_user")
-
-         
 
           if (data !== undefined) {
             // We only want to hide if the variable is defined
@@ -53,21 +46,8 @@ $(document).ready(function () {
               selected_company_element.html($('option:contains(' + data + ')').val())
               company_wrapper_element.show()
 
-              selected_commodity_element.html($('option:contains(' + data + ')').text())
-              commodity_wrapper_element.show()  
-              
-              selected_user_element.html($('option:contains(' + data + ')').text())
-              user_wrapper_element.show()               
-
-              //Trying instead to go straight to a url when selecting an option BUT only #search-user autocomplete - not any of the others
-              var user_url = $('option:contains(' + data + ')').val()
-              window.location = user_url
-
-              // Mary's attempt....
-              // Change the href of the <a> in the div for showing the option value
-              document.getElementById("selected_commodity").href = ($('option:contains(' + data + ')').val())
-
-            
+              selected_commodity_element.html($('option:contains(' + data + ')').val())
+              commodity_wrapper_element.show()
               
             } else {
               company_wrapper_element.hide()
